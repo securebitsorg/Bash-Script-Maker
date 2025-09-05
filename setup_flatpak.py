ea8c7b3e3b26050467e0025c9b8846da1de0d506
marci <marcel@secure-bits.org>
2025-09-05 13:44:19 +0200

    fix: Hinzufügen der IPC-Teilen zu den Flatpak-Bau-Befehlen

---
+++ setup_flatpak.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(
+    name="bash-script-maker",
+    version="1.1.0",
+    py_modules=["bash_script_maker", "syntax_highlighter", "localization", "custom_dialogs", "assets"],
+    entry_points={
+        "console_scripts": [
+            "bash-script-maker=bash_script_maker:main",
+        ],
+    },
+    install_requires=[
+        "ttkbootstrap>=1.10.1",
+        "pygments>=2.15.1",
+    ],
+)
